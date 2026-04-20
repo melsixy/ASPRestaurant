0cb9244a021ac7f6ec6e452a57879825a4007a27
melsixy <sarafovamariela@gmail.com>
2026-04-20 22:47:17 +0300

    CSS and Gallery changes

--- ASPRestaurant/obj/Debug/net9.0/ASPRestaurant.AssemblyInfo.cs
+++ ASPRestaurant/obj/Debug/net9.0/ASPRestaurant.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5630af9a22a6e0159cf24eb751c5499bda262378")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ba4ef9f67a3b9c9f9e21cbcdd967e2f8dbe6b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPRestaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPRestaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
